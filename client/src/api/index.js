9d446122356ef296de9cbf4772004bb3c2435c66
kenny555max <69280199+kenny555max@users.noreply.github.com>
2023-12-13 20:06:55 +0100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://e-commerce-bookstore-theta.vercel.app/'
+    baseURL: 'https://e-commerce-bookstore-murex.vercel.app'
 });
 
 API.interceptors.request.use((request) => {
